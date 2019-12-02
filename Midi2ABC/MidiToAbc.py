07b7fffdcd1bb6c92a2ea683eaf2e36e57c437a4
azamlynny <azamlynny@hotmail.com>
2019-12-02 11:00:19 -0500

    Added tqdm progress bar and progress messages for Midi2ABC

--- Midi2ABC/MidiToAbc.py
+++ Midi2ABC/MidiToAbc.py
@@ -5,14 +5,20 @@ dir = "./Midi_Files/"
 temp_dir = "./Temp/"
 
 # Convert all .midi files into .abc files
-for filename in os.listdir(dir):
+
+print("Converting to .abc...")
+
+for filename in tqdm(os.listdir(dir)):
     if filename != '.DS_Store':
         command = "midi2abc -f ./Midi_Files/" + filename + " -o ./Temp/ABC_" + filename
         os.system(command)
 
 # Merge all .abc files into one single .abc file with double spaces seperating songs
+
+print("Compiling into one file...")
+
 file_write = open("../Data_Tunes.txt","w+")
-for filename in os.listdir(temp_dir):
+for filename in tqdm(os.listdir(temp_dir)):
     if filename != '.DS_Store':
         file_read = open(os.path.join(temp_dir, filename), mode = 'r')
         data = file_read.read()
